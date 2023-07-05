6d7f2d6fb458b98f331276c281bd6c30e2983d84
Sagar Panchal <vaishalipanchal6899@gmail.com>
2023-07-05 18:18:38 -0400

    first commit

---
+++ app/src/main/java/com/example/demoexamples/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.demoexamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("kbjn","njlnk");
+    }
+}
